d210d237a27ea136832da7986a4221d772a4a2ef
Damigivenchy01 <damiridw@gmail.com>
2025-05-03 10:34:49 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'bd84e515-aa68-4b59-a988-0d6ab1ab59ed';
+export const MODAL_CLOSE_SEC = 2.5;
